8feae32454d9bd1510bf26d9229c222e207b8fe2
Ben PHL <benphawke@gmail.com>
2024-02-08 13:07:07 +0100

    setup about page in backend

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,6 +2,7 @@ use crate::Lang;
 use actix_web::{HttpResponse, Responder};
 use askama::Template;
 use fluent_templates::{static_loader, Loader};
+use unic_langid::LanguageIdentifier;
 
 #[derive(Template)]
 #[template(path = "page.html")]
@@ -35,35 +36,34 @@ static_loader! {
     };
 }
 
+fn msg_get<L: AsRef<str>>(title: &str, lang: &L) -> Option<String> {
+    LOCALES.lookup(&lang.as_ref().parse().unwrap(), title)
+}
+
 pub async fn index(lang: Lang) -> impl Responder {
-    let msg_get = |title: &str, lang: &Lang| {
-        LOCALES
-            .lookup(&lang.as_ref().parse().unwrap(), title)
-            .unwrap()
-    };
     let services = vec![
         OfferedService {
-            name: msg_get("service_3DPrinting", &lang),
-            description: msg_get("service_3DPrinting_desc", &lang),
+            name: msg_get("service_3DPrinting", &lang).unwrap(),
+            description: msg_get("service_3DPrinting_desc", &lang).unwrap(),
             link: "/fdm-3d-printing".to_string(),
         },
         OfferedService {
-            name: msg_get("service_design_optimisation", &lang),
-            description: msg_get("service_design_optimisation_desc", &lang),
+            name: msg_get("service_design_optimisation", &lang).unwrap(),
+            description: msg_get("service_design_optimisation_desc", &lang).unwrap(),
             link: "/design-optimisation".to_string(),
         },
     ];
     let idx_template = IndexBodyTemplate {
-        our_services: msg_get("our_services", &lang),
+        our_services: msg_get("our_services", &lang).unwrap(),
         services,
-        learn_more: msg_get("learn_more", &lang),
+        learn_more: msg_get("learn_more", &lang).unwrap(),
     };
 
     let page = PageTemplate {
         lang: lang.as_ref().to_string(),
         main: idx_template.to_string(),
-        help_section_title: msg_get("footer_help_title", &lang),
-        contact_us: msg_get("footer_contact_us", &lang),
+        help_section_title: msg_get("footer_help_title", &lang).unwrap(),
+        contact_us: msg_get("footer_contact_us", &lang).unwrap(),
     };
 
     match page.render() {
@@ -73,5 +73,42 @@ pub async fn index(lang: Lang) -> impl Responder {
 }
 
 pub async fn faq(_lang: Lang) -> impl Responder {
+    HttpResponse::Ok().body("raw-content here")
+}
+
+#[derive(Template)]
+#[template(path = "about.html")]
+struct AboutTemplate {
+    body_title: String,
+    title_byline: String,
+
+    intro_title: String,
+    intro_content: String,
+
+    our_approach_title: String,
+    our_approach_content: String,
+
+    why_us_title: String,
+    why_us_content: String,
+}
+
+impl AboutTemplate {
+    fn fetch_lang(lang: &Lang) -> Self {
+        Self {
+            body_title: msg_get("body_title", lang).unwrap(),
+            title_byline: msg_get("title_byline", lang).unwrap(),
+
+            intro_title: msg_get("intro_title", lang).unwrap(),
+            intro_content: msg_get("intro_content", lang).unwrap(),
+
+            our_approach_title: msg_get("our_approach_title", lang).unwrap(),
+            our_approach_content: msg_get("our_approach_content", lang).unwrap(),
+
+            why_us_title: msg_get("why_us_title", lang).unwrap(),
+            why_us_content: msg_get("why_us_content", lang).unwrap(),
+        }
+    }
+}
+pub async fn about(_lang: Lang) -> impl Responder {
     HttpResponse::Ok().body("raw-content here")
 }
