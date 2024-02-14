bf0e02ca0d398f4a7153a874aa12012ae2332f43
Ben PHL <benphawke@gmail.com>
2024-02-14 15:41:23 +0100

    Use config file

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,59 @@ use std::collections::HashSet;
 
 use actix_files as fs;
 use actix_web::{middleware, web, App, HttpServer};
+use config::{Config as CfgLoader, ConfigError, File};
 
 mod handlers;
 mod langs;
 use langs::LangGuardRedir;
 
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Config {
+    log_level: log::Level,
+    statics_path: (String, String),
+    supported_languages: HashSet<String>,
+    listen_addr: std::net::SocketAddr,
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "info");
+    let config: Config = CfgLoader::builder()
+        .add_source(config::File::with_name("./settings.toml"))
+        .build()
+        .unwrap()
+        .try_deserialize()
+        .unwrap();
+    std::env::set_var("RUST_LOG", config.log_level.as_str());
     env_logger::init();
 
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::NormalizePath::trim())
             .wrap(middleware::Logger::default())
-            .service(fs::Files::new("/static", "static").use_last_modified(true))
+            .service(
+                fs::Files::new(&config.statics_path.0, &config.statics_path.1)
+                    .use_last_modified(true),
+            )
             .service(
                 web::scope("")
-                // First: getting here means that if it's not lang'd, then 404, which implies a
-                // redirect...
-                .wrap(LangGuardRedir {
-                    supported_langs: HashSet::from([
-                        "en".to_string(),
-                        "fr".to_string(),
-                        "de".to_string(),
-                    ])
-                    .clone(),
-                })
-                .service(
-                    web::scope("/{lang}")
-                        // TODO: make putting this service here redundant.
-                        .service(fs::Files::new("/static", "static").use_last_modified(true))
-                        .route("", web::get().to(handlers::index))
-                        .route("/faq", web::get().to(handlers::faq))
-                        .route("/about_page", web::get().to(handlers::about)),
-                )
+                    // First: getting here means that if it's not lang'd, then 404, which implies a
+                    // redirect...
+                    .wrap(LangGuardRedir {
+                        supported_langs: config.supported_languages.clone(),
+                    })
+                    .service(
+                        web::scope("/{lang}")
+                            // TODO: make putting this service here redundant.
+                            .service(fs::Files::new("/static", "static").use_last_modified(true))
+                            .route("", web::get().to(handlers::index))
+                            .route("/faq", web::get().to(handlers::faq))
+                            .route("/about_page", web::get().to(handlers::about)),
+                    ),
             )
     })
-    .bind("0.0.0.0:8080")?
+    .bind(config.listen_addr)?
     .run()
     .await
 }
